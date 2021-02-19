aab5156d99aa1daecb114becdd8ba616bb3cf6b7
Tom <t.clegg17@imperial.ac.uk>
2021-02-19 10:53:30 +0000

    fix simulation tests

--- test/simulation_tests.jl
+++ test/simulation_tests.jl
@@ -10,14 +10,14 @@ end
 M,N = 1,1
 u,Rm = mat(1.0), [1.0]
 l,ρ = mat(0.2), [0.2]
-
-#Simulate
 p = make_parameters(N,M,u,Rm,l,ρ)
-@test_nowarn simulate(ones(N+M),p,t_end = 100)
+
 
 #test vs analytical prediction
 C_eq = ρ[1] / Rm[1]
 R_eq = -Rm[1] / (u[1] * (l[1] - 1))
 
-sol = simulate(ones(N+M),p,t_end = 100)
-@test all(isapprox.(sol[end],[C_eq,R_eq], rtol = 1e-2))
+@testset begin
+    sol = @test_nowarn simulate(ones(N+M),p,t_end = 100)
+    @test all(isapprox.(sol[end],[C_eq,R_eq], rtol = 1e-2))
+end
